0f20f1b2e157e8f7f4f1fd8c578ca246779023d8
James Starmer <james@starmer.com>
2016-02-11 06:36:59 -0500

    Added a deploy script

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+VERSION=$1
+
+if [ -z "$VERSION" ]; then
+    echo "usage: deploy.sh version"
+    exit 1
+fi
+
+JAVA_PID=`ps -ef | grep java | grep -v grep | awk '{print $2}'`
+
+if [ -n "$JAVA_PID" ]; then
+    echo "Stopping process $JAVA_PID"
+    kill -9 $JAVA_PID
+fi
+
+if [ -a microservice-*.jar ]; then
+    echo "Removing old microservice jar"
+    rm microservice-*.jar
+fi
+
+echo "Downloading microservice-$VERSION.jar"
+wget http://192.241.247.252/nexus/service/local/repositories/releases/content/com/kumulus/microservice/$VERSION/microservice-$VERSION.jar
+
+echo "starting up microservice-$VERSION.jar"
+nohup java -jar microservice-*.jar >/dev/null 2>&1 &
+
+echo delpoyed version: microservice-$VERSION.jar PID $!
